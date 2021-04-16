34a665db8c923d5106371a3d5b13e5aef799e275
pluu2 <paulvss2000@gmail.com>
2021-04-16 15:21:24 -0400

    various

--- src/server.py
+++ src/server.py
@@ -1,3 +1,11 @@
 import socket
 
-print('hello')
+class server(): 
+    def __init__(self,ip,socket): 
+        self.ip=ip
+        self.socket=socket
+        self.connection=0
+    def createsocket(self): 
+        self.connection =socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+        self.connection.bind(self.ip,self.socket) 
+    
